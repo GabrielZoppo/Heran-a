5ff578ac1d4528fd069b81a9585aa2d44dbf78f7
GabrielZoppo <53881684+GabrielZoppo@users.noreply.github.com>
2020-04-13 19:11:34 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,20 @@
+package Motorizados;
+
+import Motorizados.classes.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Carro onix = new Carro();
+        Carro camaro = new Carro();
+        Carro uno = new Carro();
+        Moto m = new Moto();
+        
+        m.ligar();
+        onix.ligar();
+        camaro.ligar();
+        uno.ligar();
+
+        System.out.println("Hello Java");
+    }
+}
