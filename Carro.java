4e28cfd8388d7ac403a6a7e8749e95bfdd88e739
GabrielZoppo <53881684+GabrielZoppo@users.noreply.github.com>
2020-04-13 19:14:42 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,12 @@
+package Motorizados.classes;
+
+public class Carro extends Veiculo {
+    private boolean arCondicionado;
+    private boolean bancoCouro;
+    private int tamanhoPortaMalas;
+    
+    public void marchaRe () {
+        System.out.println("O carro está andando de ré.");
+    }
+
+}
